c60145e9641c34a232262ab4880b58681909294f
Akram <akram.louati@esprit.tn>
2024-07-20 16:20:45 +0100

    IntellijClean

--- src/main/java/Tunisianet.java
+++ src/main/java/Tunisianet.java
@@ -1,8 +1,9 @@
-import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 public class Tunisianet {
     public static void main(String[] args) {
